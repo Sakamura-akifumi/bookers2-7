c5bd17d48ee0de71cc1e90e02c23bdeb0a102453
Sakamura-akifumi <tmr20th08moon@gmail.com>
2021-09-28 01:51:09 +0000

    ソート機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  scope :latest, -> {order(updated_at: :desc)}
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
